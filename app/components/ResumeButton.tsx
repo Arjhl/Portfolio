4c03b04aa9dd459aeccb118963a53c6055ddcd64
Arjun H L <arjunhl360@gmail.com>
2024-01-17 18:52:30 +0530

    Social Links are working

--- app/components/ResumeButton.tsx
+++ app/components/ResumeButton.tsx
@@ -2,15 +2,12 @@
 
 const ResumeButton = () => {
   return (
-    <h1
-      onClick={() => {
-        console.log("CLicked");
-        window.location.href =
-          "https://drive.google.com/file/d/1iwJyEMwqWcXbHfpI7qm5m3YAPGD7jiz1/view";
-      }}
+    <a
+      href="https://drive.google.com/file/d/1iwJyEMwqWcXbHfpI7qm5m3YAPGD7jiz1/view"
+      target="_blank"
     >
-      Resume📄
-    </h1>
+      <h1>Resume📄</h1>
+    </a>
   );
 };
 
